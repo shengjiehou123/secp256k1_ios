bde590d151b6eb209d18f33378f622b66b5ede77
13071806115 <>
2023-11-03 11:13:28 +0800

    fix detail

--- secp256k1_ios/contrib/lax_der_parsing.c
+++ secp256k1_ios/contrib/lax_der_parsing.c
@@ -5,7 +5,7 @@
  **********************************************************************/
 
 #include <string.h>
-#include <secp256k1.h>
+#include <secp256k1_1.h>
 
 #include "lax_der_parsing.h"
 
--- secp256k1_ios/contrib/lax_der_parsing.h
+++ secp256k1_ios/contrib/lax_der_parsing.h
@@ -51,7 +51,7 @@
 #ifndef SECP256K1_CONTRIB_LAX_DER_PARSING_H
 #define SECP256K1_CONTRIB_LAX_DER_PARSING_H
 
-#include <secp256k1.h>
+#include <secp256k1_1.h>
 
 #ifdef __cplusplus
 extern "C" {
